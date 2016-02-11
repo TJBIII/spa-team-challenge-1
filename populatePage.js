bb3cd603ffa13fd85ae88225fa0a45d7feabe0c1
williamhickok11 <williamhickok11@gmail.com>
2016-02-11 14:04:05 -0600

    add innerHTML

--- populatePage.js
+++ populatePage.js
@@ -2,8 +2,19 @@ function populatePage (contacts) {
   console.log("contacts:", contacts);
   var containerEl = document.getElementById("container");
   
+  var containerElString = "";
   //Initialize DOM with Hidden Classes - only first class will be displayed//
-  
+  for (var letter in contacts) {
+    containerElString += `<div class="row" id="${letter}">`;
+      for (var i = 0; i < contacts[letter].length; i++) {
+        var name = (contacts[letter][i].last_name || contacts[letter][i].business);
+        console.log("name", name);
+        containerElString += `<div class="col-md-12">${name}</div>`;
+      };
+    containerElString += `</div>`;
+  }
+    
+  containerEl.innerHTML = containerElString;
 }
 
 
