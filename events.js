c3bc5b2ec9fc243e1beb692173bfc70c50530370
Thomas Buida <thomasbuida@gmail.com>
2016-02-15 14:33:21 -0600

    Implemented real time search

--- events.js
+++ events.js
@@ -50,6 +50,18 @@ submitButton.addEventListener("click", function(event) {
   	ContactBook.hideAllLetterDivs();
 		// Compares user input to business/last names and changes the DOM so only the associated letter and contact appears
   	ContactBook.searchContacts();
-		
 	}
 })
+
+//real time search
+searchEl.addEventListener("keyup", function(event){
+  ContactBook.hideAllLetterDivs();
+    // Compares user input to business/last names and changes the DOM so only the associated letter and contact appears
+  if (searchEl.value){
+    ContactBook.searchContacts(); 
+  } else {
+    ContactBook.refreshToolbar();
+  }
+})
+
+
